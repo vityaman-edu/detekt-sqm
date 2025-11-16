ec6658cb1142af01b89acb22693553f61e5eae51
Victor Smirnov <53015676+vityaman@users.noreply.github.com>
2025-11-16 09:00:38 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    kotlin("jvm") version "2.2.21"
+    `maven-publish`
+}
+
+group = "org.example.detekt"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    compileOnly("io.gitlab.arturbosch.detekt:detekt-api:1.23.8")
+
+    testImplementation("io.gitlab.arturbosch.detekt:detekt-test:1.23.8")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+    testImplementation(platform("org.junit:junit-bom:5.13.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+    systemProperty("junit.jupiter.testinstance.lifecycle.default", "per_class")
+    systemProperty("compile-snippet-tests", project.hasProperty("compile-test-snippets"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
