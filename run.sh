b067c97a673525fa9cedc7f299bbb4e40cc53a19
vityaman <vityaman.dev@yandex.ru>
2025-12-17 20:12:42 +0300

    Run with a BindingContext

--- run.sh
+++ run.sh
@@ -6,8 +6,11 @@ cd "$(dirname "$0")" || exit
 clear
 gradle build
 clear
+(cd "$1"; gradle build)
+clear
 detekt \
-  --input "$1" \
+  --input "$1/src" \
+  --classpath "$1/build/classes/kotlin/main" \
   --plugins build/libs/detekt-sqm-0.1.0.jar \
   --disable-default-rulesets \
   --all-rules \
